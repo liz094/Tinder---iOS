345bc9dfdcbc279958e261bc86516d6d138b0428
Lin Zhou <zhoulin@lins-mbp.attlocal.net>
2017-04-26 16:55:48 -0700

    Initial Commit

---
+++ Tinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Lin Zhou on 4/26/17.
+//  Copyright © 2017 Lin Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
